38b6d851ed708fe3e6ecdac39eefc300b50181c3
ANUBIS4466 <134639912+ANUBIS4466@users.noreply.github.com>
2023-05-28 16:47:39 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,10 +1,24 @@
-#Sidorenko - 1
+#4 
 
-#Fedaev - 2
+	#Игровая сцена:
+	back = (0, 255, 0) 
+	win_width = 600
+	win_height = 500
+	window = display.set_mode((win_width, win_height))
+	window.fill(back)
 
-#Zhiharev - 3
+	#флаги	
+	game = True
+	finish = False
+	clock =	time.Clock()
+	FPS = 60
 
-#3ralenno - 4
+	# мяч и ракетка
+	racketl = Player('racket.png', 30, 200, 4, 50, 150) 
+	ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
-
+	font.init()
+  font = font.Font('calibri.ttf', 35)
+	losel	=	font.render('PLAYER	1	LOSE!',	True,	(180,	0,	0))
+	lose2	=	font.render('PLAYER	2	LOSE!',	True,	(180,	0,	0))
 
