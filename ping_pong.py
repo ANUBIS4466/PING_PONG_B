878820a5e11a7c2e14aa789979bb183483532c7e
ANUBIS4466 <134639912+ANUBIS4466@users.noreply.github.com>
2023-05-28 16:56:30 +0300

    Update ping_pong.py
    фы

--- ping_pong.py
+++ ping_pong.py
@@ -21,7 +21,19 @@ class GameSprite(sprite.Sprite):
 #Fedaev - 2
 
 #Zhiharev - 3
-
+while game:	
+    for e in event.get():	
+        if e.type == QUIT:
+    game = False
+    if finish != True:	
+        window.fill(back)
+        racketl.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racketl, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
 #3ralenno - 4
 
 
